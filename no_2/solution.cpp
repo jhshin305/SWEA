f8a5faaa4aa700a641982623fc4a7754598bc36c
JongHoon <jhshin305@gmail.com>
2025-01-17 17:54:52 +0900

    init

---
+++ no_2/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void init(int W, int H)
+{
+
+}
+
+int dropBlocks(int mPlayer, int mCol)
+{
+	return -1;
+}
+
+int changeBlocks(int mPlayer, int mCol)
+{
+	return -1;
+}
+
+int getResult(int mBlockCnt[2])
+{
+	return -1;
+}
