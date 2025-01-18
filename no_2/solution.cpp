c05376d9a9019200d8a4bb913e12a352a07c46a6
JongHoon <jhshin305@gmail.com>
2025-01-18 22:33:25 +0900

    final

--- no_2/solution.cpp
+++ no_2/solution.cpp
@@ -2,39 +2,39 @@
 
 using namespace std;
 
+const int MAX_W = 200;
+const int MAX_H = 200;
+
 int MoveAndDelete(int mPlayer);
 void MoveToBottom();
-vector<int> deleteBlocks(int mPlayer);
-vector<int> setZero(int mPlayer);
+void deleteBlocks(int mPlayer, int blocks[3]);
 int deleteVertical(int mh, int mw);
 int deleteHorizontal(int mh, int mw);
 int deleteDownwardRightDiagonal(int mh, int mw);
-int deleteDownwardLeftDiagonal(int mh, int mw);
-
-int w, h;
-vector<vector<int>> board;
-vector<int> scores;
+int deleteUpwardRightDiagonal(int mh, int mw);
 
 struct Point {
 	int x, y;
 };
 
-void printBoard() {
-	for(const auto& row : board) {
-		for(const auto& block : row) {
-			cout << block << " ";
-		}
-		cout << endl;
-	}
-	cout << endl;
-}
+int w, h;
+int board[MAX_H][MAX_W];
+int scores[3];
+int target;
+int Opponent;
+Point curr;
+int ni;
+int nj;
+int cnt;
 
 void init(int W, int H)
 {
 	w = W;
 	h = H;
-	board = vector<vector<int>>(h, vector<int>(w, 0));
-	scores = {0, 0, 0};
+	memset(board, 0, sizeof(board));
+	scores[0] = 0;
+	scores[1] = 0;
+	scores[2] = 0;
 }
 
 int dropBlocks(int mPlayer, int mCol)
@@ -48,24 +48,24 @@ int dropBlocks(int mPlayer, int mCol)
 
 int changeBlocks(int mPlayer, int mCol)
 {
-	int Opponent = mPlayer == 1 ? 2 : 1;
-	queue<Point> q;
-	if(board[h-1][mCol] == Opponent) q.push({h-1, mCol});
-	while(!q.empty()) {
-		Point curr = q.front();
+	Opponent = mPlayer == 1 ? 2 : 1;
+	stack<Point> s;
+	if(board[h-1][mCol] == Opponent) s.push({h-1, mCol});
+	while(!s.empty()) {
+		curr = s.top();
 		board[curr.x][curr.y] = mPlayer;
-		q.pop();
+		s.pop();
 		if(curr.x-1 >= 0 && board[curr.x-1][curr.y] == Opponent) {
-			q.push({curr.x-1, curr.y});
+			s.push({curr.x-1, curr.y});
 		}
 		if(curr.y-1 >= 0 && board[curr.x][curr.y-1] == Opponent) {
-			q.push({curr.x, curr.y-1});
+			s.push({curr.x, curr.y-1});
 		}
 		if(curr.y+1 < w && board[curr.x][curr.y+1] == Opponent) {
-			q.push({curr.x, curr.y+1});
+			s.push({curr.x, curr.y+1});
 		}
 		if(curr.x+1 < h && board[curr.x+1][curr.y] == Opponent) {
-			q.push({curr.x+1, curr.y});
+			s.push({curr.x+1, curr.y});
 		}
 	}
 
@@ -76,9 +76,11 @@ int getResult(int mBlockCnt[2])
 {
 	mBlockCnt[0] = 0;
 	mBlockCnt[1] = 0;
-	for(const auto& row : board) {
-		for(const auto& block : row) {
-			mBlockCnt[abs(block)-1]++;
+	for(int j=0; j<w; j++) {
+		for(int i=h-1; i>=0; i--) {
+			if(board[i][j] != 0) {
+				mBlockCnt[abs(board[i][j])-1]++;
+			} else break;
 		}
 	}
 	return scores[1]>scores[2] ? 1 : scores[1]<scores[2] ? 2 : 0;
@@ -89,13 +91,12 @@ int MoveAndDelete(int mPlayer) {
 
 	while(true) {
 		MoveToBottom();
-		vector<int> cnt = deleteBlocks(mPlayer);
-		if(cnt == vector<int> {0, 0, 0}) break;
-		else score += cnt[mPlayer];
+		int blocks[3] = {0, 0, 0};
+		deleteBlocks(mPlayer, blocks);
+		if(blocks[0] == 0 && blocks[1] == 0 && blocks[2] == 0) break;
+		else score += blocks[mPlayer];
 	}
 	scores[mPlayer] += score;
-	
-	// printBoard();//////////////////////////////////////////////////////
 	
 	return score;
 }
@@ -103,64 +104,49 @@ int MoveAndDelete(int mPlayer) {
 void MoveToBottom() {
 	for(int c=0; c<w; c++) {
 		int bottom = h-1;
-		int top = h-1;
-		while(top >= 0) {
-			if(board[top][c] == 0) {
-				top--;
-			} else {
+		for (int top = h - 1; top >= 0; top--) {
+			if(board[top][c] != 0) {
 				board[bottom][c] = board[top][c];
 				if(top != bottom) {
 					board[top][c] = 0;
 				}
 				bottom--;
-				top--;
 			}
 		}
 	}
 }
 
-vector<int> deleteBlocks(int mPlayer) {
-	vector<int> cnt = {0, 0, 0};
-	for(int i=0; i<h; i++) {
-		for(int j=0; j<w; j++) {
+void deleteBlocks(int mPlayer, int blocks[3]) {
+	for(int j=0; j<w; j++) {
+		for(int i=h-1; i>=0; i--) {
 			if(board[i][j] != 0) {
-				if(i==0 || abs(board[i-1][j]) != abs(board[i][j])) deleteVertical(i, j);
+				if(i==h-1 || abs(board[i+1][j]) != abs(board[i][j])) deleteVertical(i, j);
 				if(j==0 || abs(board[i][j-1]) != abs(board[i][j])) deleteHorizontal(i, j);
 				if(i==0 || j==0 || abs(board[i-1][j-1]) != abs(board[i][j])) deleteDownwardRightDiagonal(i, j);
-				if(i==0 || j==w-1 || abs(board[i-1][j+1]) != abs(board[i][j])) deleteDownwardLeftDiagonal(i, j);
+				if(i==h-1 || j==0 || abs(board[i+1][j-1]) != abs(board[i][j])) deleteUpwardRightDiagonal(i, j);
+				if(board[i][j] < 0) {
+					blocks[abs(board[i][j])]++;
+					board[i][j] = 0;
+				}
 			}
+			else break;
 		}
 	}
-	cnt = setZero(mPlayer);
-
-	return cnt;
-}
-
-vector<int> setZero(int mPlayer) {
-	vector<int> cnt = {0, 0, 0};
-	for(int i=0; i<h; i++) {
-		for(int j=0; j<w; j++) {
-			if(board[i][j] < 0) {
-				cnt[abs(board[i][j])]++;
-				board[i][j] = 0;
-			}
-		}
-	}
-	return cnt;
 }
 
 int deleteVertical(int mh, int mw) {
-	int cnt = 1;
-	int ni = mh;
-	int nj = mw;
-	while(ni+1 < h && abs(board[mh][mw]) == abs(board[ni+1][mw])) {
+	cnt = 1;
+	ni = mh;
+	nj = mw;
+	target = abs(board[mh][mw]);
+	while(ni-1 >= 0 && target == abs(board[ni-1][mw])) {
 		cnt++;
-		ni++;
+		ni--;
 	}
 	if(cnt >= 5) {
 		for(int i=0; i<cnt; i++) {
 			board[mh][mw] = -abs(board[mh][mw]);
-			mh++;
+			mh--;
 		}
 		return cnt;
 	}
@@ -168,10 +154,11 @@ int deleteVertical(int mh, int mw) {
 }
 
 int deleteHorizontal(int mh, int mw) {
-	int cnt = 1;
-	int ni = mh;
-	int nj = mw;
-	while(nj+1 < w && abs(board[mh][mw]) == abs(board[mh][nj+1])) {
+	cnt = 1;
+	ni = mh;
+	nj = mw;
+	target = abs(board[mh][mw]);
+	while(nj+1 < w && target == abs(board[mh][nj+1])) {
 		cnt++;
 		nj++;
 	}
@@ -186,10 +173,11 @@ int deleteHorizontal(int mh, int mw) {
 }
 
 int deleteDownwardRightDiagonal(int mh, int mw) {
-	int cnt = 1;
-	int ni = mh;
-	int nj = mw;
-	while(ni+1 < h && nj+1 < w && abs(board[mh][mw]) == abs(board[ni+1][nj+1])) {
+	cnt = 1;
+	ni = mh;
+	nj = mw;
+	target = abs(board[mh][mw]);
+	while(ni+1 < h && nj+1 < w && target == abs(board[ni+1][nj+1])) {
 		cnt++;
 		ni++;
 		nj++;
@@ -205,20 +193,21 @@ int deleteDownwardRightDiagonal(int mh, int mw) {
 	return 0;
 }
 
-int deleteDownwardLeftDiagonal(int mh, int mw) {
-	int cnt = 1;
-	int ni = mh;
-	int nj = mw;
-	while(ni+1 < h && nj-1 >= 0 && abs(board[mh][mw]) == abs(board[ni+1][nj-1])) {
+int deleteUpwardRightDiagonal(int mh, int mw) {
+	cnt = 1;
+	ni = mh;
+	nj = mw;
+	target = abs(board[mh][mw]);
+	while(ni-1 >= 0 && nj+1 < w && target == abs(board[ni-1][nj+1])) {
 		cnt++;
-		ni++;
-		nj--;
+		ni--;
+		nj++;
 	}
 	if(cnt >= 5) {
 		for(int i=0; i<cnt; i++) {
 			board[mh][mw] = -abs(board[mh][mw]);
-			mh++;
-			mw--;
+			mh--;
+			mw++;
 		}
 		return cnt;
 	}
