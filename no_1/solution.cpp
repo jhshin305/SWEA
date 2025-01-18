9002b4f29af8a38a66412566ed641da6b2e82323
JongHoon <jhshin305@gmail.com>
2025-01-18 22:53:52 +0900

    final2

--- no_1/solution.cpp
+++ no_1/solution.cpp
@@ -3,6 +3,7 @@
 
 #define MAX_N 1000
 #define MAX_M 20
+#define MAK_TREASURE 10000 
 
 using namespace std;
 
@@ -11,6 +12,8 @@ struct Result {
 };
 
 int n, m;
+Result treasures[MAX_N * MAX_N];
+int treasureCount;
 
 void spin(int Pieces[MAX_M][MAX_M], Result& target)
 {
@@ -36,20 +39,19 @@ void spin(int Pieces[MAX_M][MAX_M], Result& target)
     target.x = m - tmp_y - 1;
 }
 
-vector<Result> treasure;
 int MAP[MAX_N][MAX_N];
 
 void init(int N, int M, int Map[MAX_N][MAX_N])
 {
     n = N;
     m = M;
-    treasure.clear(); // Clear the treasure vector to avoid duplicates
+    treasureCount = 0; // Initialize treasure count
     for(int i = 0; i < N; i++)
     {
         for(int j = 0; j < N; j++)
         {
             MAP[i][j] = Map[i][j];
-            if(Map[i][j] == 1) treasure.push_back({i,j});
+            if(Map[i][j] == 1) treasures[treasureCount++] = {i, j};
         }
     }
 }
@@ -76,9 +78,11 @@ Result findTreasureChest(int Pieces[MAX_M][MAX_M])
 
     for(int s=0; s<4; s++) {//rotate
         if(s>0) spin(Pieces, target);
-        for (auto const &p : treasure) {//check all possible position of the map
-            bool check = true;
-			if(MAP[p.y][p.x] != 1) continue;
+        bool check = true;
+        for (int t = 0; t < treasureCount; t++) {//check all possible position of the map
+            check = true;
+            Result p = treasures[t];
+            if(MAP[p.y][p.x] != 1) continue;
             if(p.y - target.y < 0 || p.x - target.x < 0 || p.y - target.y + m > n || p.x - target.x + m > n) continue;
             for(int i = 0; i < m; i++)
             {
