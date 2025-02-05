68e366cf38129b8f7190ecfb6c4e6cf7f38cbfa7
JongHoon <jhshin305@gmail.com>
2025-02-05 23:25:02 +0900

    init

---
+++ 20250206/n_7_2930/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define mod % 1000000007
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	freopen("C:/Users/jhshin/Desktop/secret/swea/20250206/n_7_2930/sample_input.txt", "r", stdin);
+	
+	int T;
+	cin >> T;
+	for (int test_case=1; test_case<=T; test_case++) {
+	
+		int n;
+		cin >> n;
+
+		
+	
+	}
+	
+	return 0;
+}
