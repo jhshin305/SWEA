738b073bd4648e1f4f1a39e16cea8033eba5c4c6
JongHoon <jhshin305@gmail.com>
2025-02-05 21:58:09 +0900

    init

---
+++ 20250205/n_5_7465/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define mod % 1000000007
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	freopen("C:/Users/jhshin/Desktop/secret/swea/20250205/n_5_7465/sample_input.txt", "r", stdin);
+	
+	int T;
+	cin >> T;
+	for (int test_case=1; test_case<=T; test_case++) {
+	
+		int n, m;
+		cin >> n >> m;
+
+		int cnt = n;
+
+		for(int i=0; i<m; i++) {
+			int a, b;
+			cin >> a >> b;
+			
+		}
+
+		cout << "#" << test_case << " " << cnt << endl;
+	}
+	
+	return 0;
+}
