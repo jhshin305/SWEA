2adc44766da0c7a967bc2aa9700e633023d692eb
JongHoon <jhshin305@gmail.com>
2025-02-10 13:56:33 +0900

    init

---
+++ 20250210/n_13_16245/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int init(int N, int sCity[], int eCity[], int mCost[]) {
+	return 0;
+}
+
+void add(int sCity, int eCity, int mCost) {
+	return;
+}
+
+int cost(int mHub) {
+	return 0;
+}
