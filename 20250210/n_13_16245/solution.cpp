b94650fa5c9498e38065790db79f3893bf4adbd6
JongHoon <jhshin305@gmail.com>
2025-02-12 22:26:02 +0900

    final

--- 20250210/n_13_16245/solution.cpp
+++ 20250210/n_13_16245/solution.cpp
@@ -1,90 +1,91 @@
 #include <bits/stdc++.h>
 #define ll long long
 #define INF INT_MAX
-#define MAX_CITY 600
-#define MAX_N 1400
+#define MAX_CITY 601
 
 using namespace std;
 
-int n;
 int city_cnt;
-vector<vector<array<int, 2>>> line(MAX_CITY); // {ecity, cost}
-vector<vector<array<int, 2>>> rline(MAX_CITY); // {scity, cost}
-map<int, int> indexing;
-vector<vector<int>> dist(MAX_CITY, vector<int>(MAX_CITY, INF));
+vector<array<int, 2>> line[MAX_CITY]; // {ecity, cost}
+vector<array<int, 2>> rline[MAX_CITY]; // {scity, cost}
+int dist[MAX_CITY][MAX_CITY];
+priority_queue<array<int, 2>, vector<array<int,2>>, greater<>> pq;
 
 void dijkstra(const int& s) {
-	for(int s=0; s<city_cnt; s++) {
-		vector<int> d(MAX_CITY, INF);
-		d[s] = 0;
-		priority_queue<array<int, 2>> pq;
-		pq.push({0, s});
-		while(!pq.empty()) {
-			auto [cost, curr] = pq.top();
-			pq.pop();
-			if(d[curr] < cost) continue;
-			for(auto [next, ncost] : line[curr]) {
-				if(d[next] > cost + ncost) {
-					d[next] = cost + ncost;
-					pq.push({d[next], next});
-				}
+	for(int i=0; i<city_cnt; i++) {
+		dist[s][i] = INF;
+		dist[i][s] = INF;
+	}
+	dist[s][s] = 0;
+	pq.push({0, s});
+	while(!pq.empty()) {
+		auto [cost, curr] = pq.top();
+		pq.pop();
+		if(dist[s][curr] != cost) continue;
+		for(auto [next, ncost] : line[curr]) {
+			if(dist[s][next] > cost + ncost) {
+				dist[s][next] = cost + ncost;
+				pq.push({dist[s][next], next});
 			}
 		}
-		for(int e=0; e<city_cnt; e++) {
-			dist[s][e] = d[e];
+	}
+	pq.push({0, s});
+	while(!pq.empty()) {
+		auto [cost, curr] = pq.top();
+		pq.pop();
+		if(dist[curr][s] != cost) continue;
+		for(auto [next, ncost] : rline[curr]) {
+			if(dist[next][s] > cost + ncost) {
+				dist[next][s] = cost + ncost;
+				pq.push({dist[next][s], next});
+			}
 		}
 	}
 }
 
-int scity;
-int ecity;
+vector<int> cityMap;
+
+int findIdx(const int& x) {
+	return lower_bound(cityMap.begin(), cityMap.end(), x) - cityMap.begin();
+}
 
 int init(int N, int sCity[], int eCity[], int mCost[]) {
-	n = N;
-	city_cnt = 0;
-	// indexing = vector<int>(MAX_ADR, -1);
-	line = vector<vector<array<int, 2>>>(MAX_CITY);
-	indexing.clear();
-	dist = vector<vector<int>>(MAX_CITY, vector<int>(MAX_CITY, INF));
+	cityMap.clear();
+	for(int i=0; i<N; i++) {
+		cityMap.push_back(sCity[i]);
+		cityMap.push_back(eCity[i]);
+	}
+	sort(cityMap.begin(), cityMap.end());
+	cityMap.erase(unique(cityMap.begin(), cityMap.end()), cityMap.end());
+	city_cnt = cityMap.size();
+	for(int i=0; i<city_cnt; i++) {
+		line[i].clear();
+		rline[i].clear();
+	}
 	for(int i = 0; i < N; i++) {
-		// if(indexing[sCity[i]]) {
-		if(indexing.find(sCity[i]) == indexing.end()) {
-			indexing[sCity[i]] = city_cnt++;
-		}
-		// if(indexing[eCity[i]] == -1) {
-		if(indexing.find(eCity[i]) == indexing.end()) {
-			indexing[eCity[i]] = city_cnt++;
-		}
-		// dist[indexing[sCity[i]]][indexing[eCity[i]]] = mCost[i];
-		line[indexing[sCity[i]]].push_back({indexing[eCity[i]], mCost[i]});
+		sCity[i] = findIdx(sCity[i]);
+		eCity[i] = findIdx(eCity[i]);
+		line[sCity[i]].push_back({eCity[i], mCost[i]});
+		rline[eCity[i]].push_back({sCity[i], mCost[i]});
 	}
 
 	return city_cnt;
 }
 
 void add(int sCity, int eCity, int mCost) {
-	// scity = indexing[sCity];
-	// ecity = indexing[eCity];
-	// if(dist[scity][ecity] > mCost) {
-	// 	dist[scity][ecity] = mCost;
-	// 	for(int s=0; s<city_cnt; s++) {
-	// 		for(int e=0; e<city_cnt; e++) {
-	// 			dist[s][e] = min(dist[s][e], dist[s][scity] + mCost + dist[ecity][e]);
-	// 		}
-	// 	}
-	// }
-	line[indexing[sCity]].push_back({indexing[eCity], mCost});
+	sCity = findIdx(sCity);
+	eCity = findIdx(eCity);
+	line[sCity].push_back({eCity, mCost});
+	rline[eCity].push_back({sCity, mCost});
 	return;
 }
 
 int cost(int mHub) {
 	ll ans = 0;
-	dijkstra(mHub);
+	int s = findIdx(mHub);
+	dijkstra(s);
 	for(int i=0; i<city_cnt; i++) {
-		ans += dist[indexing[mHub]][i] + dist[i][indexing[mHub]];
-		// cout << "i: " << i << " mHub: " << indexing[mHub] << endl;
-		// cout << dist[indexing[mHub]][i] << " " << dist[i][indexing[mHub]] << " " << dist[indexing[mHub]][i] + dist[i][indexing[mHub]] << endl;
-		// cout << endl;
+		ans += dist[s][i] + dist[i][s];
 	}
 	return ans;
 }
