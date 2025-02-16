afc50f3d6023b4cbe1439611e6e76bc1a9e1170f
JongHoon <jhshin305@gmail.com>
2025-02-16 14:28:09 +0900

    init

---
+++ 20250212_Trie/n_17_14707/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+void init() {
+	return;
+}
+
+int add(char str[]) {
+	return 0;
+}
+
+int remove(char str[]) {
+	return 0;
+}
+
+int search(char str[]) {
+	return 0;
+}
