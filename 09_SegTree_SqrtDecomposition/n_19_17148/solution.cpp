31697c7994b4ffec8698bc72c71be5f2d1276b22
JongHoon <jhshin305@gmail.com>
2025-02-18 16:00:28 +0900

    init

---
+++ 09_SegTree_SqrtDecomposition/n_19_17148/solution.cpp
@@ -0,0 +1,24 @@
+void init(int N, int M, int mType[], int mTime[])
+{
+
+}
+
+void destroy()
+{
+
+}
+
+void update(int mID, int mNewTime)
+{
+
+}
+
+int updateByType(int mTypeID, int mRatio256)
+{
+	return -1;
+}
+
+int calculate(int mA, int mB)
+{
+	return -1;
+}
