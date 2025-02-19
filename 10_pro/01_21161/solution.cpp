c9c336c05324eaf68315e28d056792d2035f56db
JongHoon <jhshin305@gmail.com>
2025-02-19 13:42:02 +0900

    init

---
+++ 10_pro/01_21161/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+void init(int N, int K, int mNodeA[], int mNodeB[], int mTime[])
+{
+}
+
+void addLine(int mNodeA, int mNodeB, int mTime)
+{
+}
+
+void removeLine(int mNodeA, int mNodeB)
+{
+}
+
+int checkTime(int mNodeA, int mNodeB)
+{
+	return 0;
+}
